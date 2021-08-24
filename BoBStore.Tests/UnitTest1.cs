6aa7a2f782dfce4ca02330bed8e5959f297fbb76
fernandoareias <nando.calheirosx@gmail.com>
2021-08-24 20:36:29 -0300

    Test

--- BoBStore.Tests/UnitTest1.cs
+++ BoBStore.Tests/UnitTest1.cs
@@ -1,7 +1,6 @@
-using BoBStore.Domain.StoreContext.Entities;
-using BoBStore.Domain.StoreContext.ValueObjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using BoBStore.Domain.StoreContext.ValueObjects;
+using BoBStore.Domain.StoreContext.Entities;
 namespace BoBStore.Tests
 {
     [TestClass]
@@ -10,10 +9,10 @@ namespace BoBStore.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            var name = new Name("Fernando", "Areias");
-            var document = new Document("14270066792");
-            var email = new Email("nando.calheirosx@gmail.com");
-            var client = new Customer(name, document, email, "21992546466");
+            var name = new Name("Bob", "Bob");
+            var document = new Document("10089556812");
+            var email = new Email("Bob.bob@gmail.com");
+            var client = new Customer(name, document, email, "21992564895");
             var mouse = new Product("Mouse", "Rato", 59.90M, "img.png", 10);
             var order = new Order(client);
             order.AddItem(new OrderItem(mouse, 5));
