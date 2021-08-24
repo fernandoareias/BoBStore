051a427cded4f4194bb228a42df29a4ac221c3e2
fernandoareias <nando.calheirosx@gmail.com>
2021-08-23 22:08:02 -0300

    Add Flunt and Ship()/Cancel()

--- BoBStore.Domain/StoreContext/Entities/Delivery.cs
+++ BoBStore.Domain/StoreContext/Entities/Delivery.cs
@@ -2,10 +2,11 @@
 
 using System;
 using BoBStore.Domain.StoreContext.Enums;
+using Flunt.Notifications;
 
 namespace BoBStore.Domain.StoreContext.Entities
 {
-    public class Delivery
+    public class Delivery : Notifiable<Notification>
     {
         public Delivery(DateTime estimatedDeliveryDate)
         {
@@ -14,9 +15,20 @@ namespace BoBStore.Domain.StoreContext.Entities
             Status = EDeliveryStatus.Awaiting;
         }
 
+
         public DateTime CreateDateUpdate { get; private set; }
         public DateTime EstimatedDeliveryDate { get; private set; }
         public EDeliveryStatus Status { get; private set; }
 
+        public void Ship()
+        {
+            // Se data de entrega for passada, nao entregar.
+            Status = EDeliveryStatus.Shipped;
+        }
+
+        public void Cancel()
+        {
+            Status = EDeliveryStatus.Canceled;
+        }
     }
 }
