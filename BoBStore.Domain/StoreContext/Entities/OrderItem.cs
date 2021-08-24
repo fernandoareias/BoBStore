d1bc82ef899dd6b6aaa44e92e131cac295145d24
fernandoareias <nando.calheirosx@gmail.com>
2021-08-23 22:08:44 -0300

    Add Flunt and Notification

--- BoBStore.Domain/StoreContext/Entities/OrderItem.cs
+++ BoBStore.Domain/StoreContext/Entities/OrderItem.cs
@@ -1,20 +1,24 @@
 
 
 using System;
+using Flunt.Notifications;
 
 namespace BoBStore.Domain.StoreContext.Entities
 {
-    public class OrderItem
+    public class OrderItem : Notifiable<Notification>
     {
-        public OrderItem(Product product, string quantity)
+        public OrderItem(Product product, int quantity)
         {
             Product = product;
             Quantity = quantity;
             Price = product.Price;
+
+            if (product.Quantity < quantity)
+                AddNotification("Quantity", "Produto fora de estoque");
         }
 
         public Product Product { get; private set; }
-        public string Quantity { get; private set; }
+        public int Quantity { get; private set; }
         public decimal Price { get; private set; }
     }
 }
