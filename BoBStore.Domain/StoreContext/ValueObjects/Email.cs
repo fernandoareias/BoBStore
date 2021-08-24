13f1a901f58c993a23fd2836e9bda683a0902c8e
fernandoareias <nando.calheirosx@gmail.com>
2021-08-24 20:35:35 -0300

    Add Notifications

--- BoBStore.Domain/StoreContext/ValueObjects/Email.cs
+++ BoBStore.Domain/StoreContext/ValueObjects/Email.cs
@@ -1,12 +1,20 @@
 
 
+using Flunt.Notifications;
+using Flunt.Validations;
+
 namespace BoBStore.Domain.StoreContext.ValueObjects
 {
-    public class Email
+    public class Email : Notifiable<Notification>
     {
         public Email(string address)
         {
             Address = address;
+
+            AddNotifications(new Contract<Notification>()
+             .Requires()
+             .IsEmail(Address, "Email", "O E-mail é invalido.")
+         );
         }
 
         public string Address { get; private set; }
