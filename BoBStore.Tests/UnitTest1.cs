8eacf0267720c5c1508cacbada2b96e1bfc74df9
Fernando <87771786+fernandoareias@users.noreply.github.com>
2021-08-23 22:26:43 -0300

    Update UnitTest1.cs

--- BoBStore.Tests/UnitTest1.cs
+++ BoBStore.Tests/UnitTest1.cs
@@ -10,10 +10,10 @@ namespace BoBStore.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            var name = new Name("Fernando", "Areias");
-            var document = new Document("14270066792");
-            var email = new Email("nando.calheirosx@gmail.com");
-            var client = new Customer(name, document, email, "21992546466");
+            var name = new Name("Bob", "Bob");
+            var document = new Document("19265886599");
+            var email = new Email("bob.bob@gmail.com");
+            var client = new Customer(name, document, email, "21992546468");
             var mouse = new Product("Mouse", "Rato", 59.90M, "img.png", 10);
             var order = new Order(client);
             order.AddItem(new OrderItem(mouse, 5));
