675fc2f00fe0158135df35671c14b90a413f3273
fernandoareias <nando.calheirosx@gmail.com>
2021-08-25 19:06:27 -0300

    Fail Fast Validations

--- BoBStore.Domain/StoreContext/Commands/CustomerCommands/Inputs/CreateCustomerCommand.cs
+++ BoBStore.Domain/StoreContext/Commands/CustomerCommands/Inputs/CreateCustomerCommand.cs
@@ -1,13 +1,31 @@
 
 
+using Flunt.Notifications;
+using Flunt.Validations;
+
 namespace BoBStore.Domain.StoreContext.CustomerCommands.Inputs
 {
-    public class CreateCustomerCommand
+    public class CreateCustomerCommand : Notifiable<Notification>
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Document { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+
+        //  Fail Fast Validations 
+        public bool Valid()
+        {
+            AddNotifications(new Contract<Notification>()
+               .Requires()
+               .IsLowerThan(FirstName, 42, "FirstName", "O nome não pode ser maior que 42 caractéres.")
+               .IsGreaterThan(FirstName, 3, "FirstName", "O nome deve ser maior que 3 caractéres")
+               .IsLowerThan(LastName, 42, "LastName", "O sobrenome deve ser menor que 42 caractéres.")
+               .IsGreaterThan(LastName, 3, "LastName", "O sobrenome deve ser maior que 3 caractéres.")
+               .IsEmail(Email, "Email", "O E-mail é invalido.")
+               .IsLowerThan(Document, 11, "Document", "CPF Inválido.")
+           );
+            return Valid();
+        }
     }
 }
