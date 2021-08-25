440234efb928ecfb07507c7c8f88985d0f4fa829
fernandoareias <nando.calheirosx@gmail.com>
2021-08-25 19:26:03 -0300

    Implemented Interface & Start List<OrdemItemCommands> in Constructor

--- BoBStore.Domain/StoreContext/Commands/OrderCommands/Inputs/CreateOrderCommands.cs
+++ BoBStore.Domain/StoreContext/Commands/OrderCommands/Inputs/CreateOrderCommands.cs
@@ -2,13 +2,32 @@
 
 using System;
 using System.Collections.Generic;
+using BoBStore.Shared.Commands;
+using Flunt.Notifications;
+using Flunt.Validations;
 
 namespace BoBStore.Domain.StoreContext.OrderCommands.Inputs
 {
-    public class CreateOrderCommands
+    public class CreateOrderCommands : Notifiable<Notification>, ICommands
     {
+        public CreateOrderCommands()
+        {
+            OrderItens = new List<OrderItemCommands>();
+        }
         public Guid Customer { get; set; }
         public IList<OrderItemCommands> OrderItens { get; set; }
+
+        public bool Valid()
+        {
+            AddNotifications(new Contract<Notification>()
+                .Requires()
+                .IsLowerThan(Customer.ToString(), 37, "Customer", "O ID do cliente está inválido.")
+                .IsGreaterThan(Customer.ToString(), 35, "Customer", "O ID do cliente está inválido.")
+                .IsGreaterThan(OrderItens.Count, 0, "Items", "Nenhum item do pedido foi encontrado.")
+
+            );
+            return Valid();
+        }
     }
 
     public class OrderItemCommands
@@ -16,4 +35,6 @@ namespace BoBStore.Domain.StoreContext.OrderCommands.Inputs
         public Guid Product { get; set; }
         public int Quantity { get; set; }
     }
+
+
 }
