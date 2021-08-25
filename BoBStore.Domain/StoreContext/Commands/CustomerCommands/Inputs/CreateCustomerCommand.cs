c25162f7ee1cfc21a1937254106cca19461b7d3d
fernandoareias <nando.calheirosx@gmail.com>
2021-08-25 18:18:10 -0300

    Create Customer Command

---
+++ BoBStore.Domain/StoreContext/Commands/CustomerCommands/Inputs/CreateCustomerCommand.cs
@@ -0,0 +1,13 @@
+
+
+namespace BoBStore.Domain.StoreContext.CustomerCommands.Inputs
+{
+    public class CreateCustomerCommand
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
