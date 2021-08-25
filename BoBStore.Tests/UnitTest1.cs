7bb8d4a7d9e5a3557738a04dfdc6d3f7b4ad2777
fernandoareias <nando.calheirosx@gmail.com>
2021-08-25 15:34:28 -0300

    Test

--- BoBStore.Tests/UnitTest1.cs
+++ BoBStore.Tests/UnitTest1.cs
@@ -10,18 +10,13 @@ namespace BoBStore.Tests
         public void TestMethod1()
         {
             var name = new Name("Bob", "Bob");
-<<<<<<< HEAD
-            var document = new Document("10089556812");
-            var email = new Email("Bob.bob@gmail.com");
-            var client = new Customer(name, document, email, "21992564895");
-=======
+
             var document = new Document("19265886599");
             var email = new Email("bob.bob@gmail.com");
             var client = new Customer(name, document, email, "21992546468");
->>>>>>> 8eacf0267720c5c1508cacbada2b96e1bfc74df9
             var mouse = new Product("Mouse", "Rato", 59.90M, "img.png", 10);
             var order = new Order(client);
-            order.AddItem(new OrderItem(mouse, 5));
+            order.AddItem(mouse, 5);
 
             // Gera o pedido
             order.Place();
