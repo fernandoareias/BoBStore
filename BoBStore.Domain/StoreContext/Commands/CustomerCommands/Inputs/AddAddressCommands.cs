06016739a4e01089a689457da74a205b8e664387
fernandoareias <nando.calheirosx@gmail.com>
2021-08-26 11:25:08 -0300

    Implemented ICommand

--- BoBStore.Domain/StoreContext/Commands/CustomerCommands/Inputs/AddAddressCommands.cs
+++ BoBStore.Domain/StoreContext/Commands/CustomerCommands/Inputs/AddAddressCommands.cs
@@ -2,10 +2,12 @@
 
 using System;
 using BoBStore.Domain.StoreContext.Enums;
+using BoBStore.Shared.Commands;
+using Flunt.Notifications;
 
 namespace BoBStore.Domain.StoreContext.CustomerCommands.Inputs
 {
-    public class AddAddressCommands
+    public class AddAddressCommands : Notifiable<Notification>, ICommands
     {
         public Guid Id { get; set; }
         public string Street { get; private set; }
@@ -17,5 +19,10 @@ namespace BoBStore.Domain.StoreContext.CustomerCommands.Inputs
         public string Country { get; private set; }
         public string ZipCode { get; private set; }
         public EAddressType Type { get; private set; }
+
+        public bool Valid()
+        {
+            return IsValid;
+        }
     }
 }
