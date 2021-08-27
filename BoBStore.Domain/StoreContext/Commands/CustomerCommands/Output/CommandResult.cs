440ccf84029c751fa8fef478c0f7cc9b03c80e6e
fernandoareias <nando.calheirosx@gmail.com>
2021-08-27 14:04:20 -0300

    Command Result

---
+++ BoBStore.Domain/StoreContext/Commands/CustomerCommands/Output/CommandResult.cs
@@ -0,0 +1,20 @@
+using System;
+using BoBStore.Shared.Commands;
+
+
+namespace BoBStores.Domain.StoreContext.CustomerCommands.Inputs
+{
+    public class CommandResult : ICommandResult
+    {
+        public CommandResult(bool success, string message, object data)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+    }
+}
