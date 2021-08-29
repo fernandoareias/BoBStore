d13f58ea0e429f44bfe18b882e663ce90e41b71e
fernandoareias <nando.calheirosx@gmail.com>
2021-08-29 19:01:34 -0300

    ConnectionString

---
+++ BoBStore.Shared/Settings.cs
@@ -0,0 +1,9 @@
+
+
+namespace BoBStore.Shared
+{
+    public static class Settings
+    {
+        public static string ConnectionString = "server=localhost,1433;database=BoBStore;User ID=sa;Password=aaa";
+    }
+}
