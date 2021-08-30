b61cce45aaa045647d3e6a5d039db893c6df8046
fernandoareias <nando.calheirosx@gmail.com>
2021-08-30 14:52:04 -0300

    Static class Settings for ConnectionString

--- BoBStore.Shared/Settings.cs
+++ BoBStore.Shared/Settings.cs
@@ -4,6 +4,6 @@ namespace BoBStore.Shared
 {
     public static class Settings
     {
-        public static string ConnectionString = "server=localhost,1433;database=BoBStore;User ID=sa;Password=aaa";
+        public static string ConnectionString = "server=localhost,1433;database=BoBStore;User ID=sa;Password=Nando@37074957";
     }
 }
