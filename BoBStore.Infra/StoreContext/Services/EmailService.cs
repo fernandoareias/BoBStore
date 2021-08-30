c54c13ba4a27c078cae4a5b8b3b7d227b8b83622
fernandoareias <nando.calheirosx@gmail.com>
2021-08-30 19:00:45 -0300

    Implemented IEmailService

---
+++ BoBStore.Infra/StoreContext/Services/EmailService.cs
@@ -0,0 +1,14 @@
+
+
+using BoBStore.Domain.StoreContext.Services;
+
+namespace BobStore.Infra.StoreContext.Services
+{
+    public class EmailService : IEmailService
+    {
+        public void Send(string to, string from, string subject, string body)
+        {
+            //Implementar
+        }
+    }
+}
