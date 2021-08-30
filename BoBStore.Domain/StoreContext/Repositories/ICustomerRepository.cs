8c4e3c49e76c5f1f240065a8c30962d73f93764a
fernandoareias <nando.calheirosx@gmail.com>
2021-08-30 14:50:50 -0300

    Add CustomerOrdersCountResult

--- BoBStore.Domain/StoreContext/Repositories/ICustomerRepository.cs
+++ BoBStore.Domain/StoreContext/Repositories/ICustomerRepository.cs
@@ -1,6 +1,7 @@
 
 
 using BoBStore.Domain.StoreContext.Entities;
+using BoBStore.Domain.StoreContext.Queries;
 
 namespace BoBStore.Domain.StoreContext.Repositories
 {
@@ -9,5 +10,6 @@ namespace BoBStore.Domain.StoreContext.Repositories
         bool CheckDocument(string document);
         bool CheckEmail(string email);
         void Save(Customer customer);
+        CustomerOrdersCountResult GetCustomerOrdersCount(string document);
     }
 }
